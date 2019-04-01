ec6666f9de3f7d7426d2c30ce35ff0b6e312f716
Chris Coyier <chriscoyier@gmail.com>
2019-04-01 13:30:27 -0700

    nav fixes

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -16,12 +16,15 @@ const Nav = location => {
             <Link to="services/cmss">CMSs</Link>
             <Link to="services/notifications">Notifications</Link>
             <Link to="services/ssgs">Static Site Generators</Link>
+            <Link to="services/hosting">Hosting</Link>
+            <Link to="services/media">Media</Link>
+            <Link to="services/monitoring">Monitoring</Link>
           </div>
         </li>
-        <li>
+        <li data-active={loc.pathname.includes("/resources/") ? "true" : ""}>
           <Link to="/resources/">Resources</Link>
         </li>
-        <li>
+        <li data-active={loc.pathname.includes("/ideas/") ? "true" : ""}>
           <Link to="/ideas/">Ideas</Link>
         </li>
       </ul>
