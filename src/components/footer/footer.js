7522b01893a5b5024657cad7b82af4dd9e6bd915
Chris Coyier <chriscoyier@gmail.com>
2019-05-10 07:11:46 -0700

    more footer crap

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,8 +5,25 @@ import styles from "./footer.module.scss";
 
 const Footer = () => (
   <footer class={styles.homeFooter}>
-    <h3>What is Serverless?</h3> Somebody else's computers.{" "}
-    <Link to="/about/">Find out more.</Link>
+    <h3 class={styles.whatis}>Yeah, but what is serverless?</h3>
+    <div className={styles.findOutWrap}>
+      <Link to="/about/" className={styles.findOut}>
+        Find out →
+      </Link>
+    </div>
+    <br />
+    <img src="/img/footer-sep.svg" alt="" />
+    <p>
+      I'm a serverless noob. Got a correction? Something to add?{" "}
+      <a href="https://github.com/CSS-Tricks/serverless">
+        There is a GitHub Repo
+      </a>{" "}
+      where you can contribute or open an issue.
+    </p>
+    <p>
+      Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> and hosted on{" "}
+      <a href="https://www.netlify.com/">Netlify</a>.
+    </p>
   </footer>
 );
 
