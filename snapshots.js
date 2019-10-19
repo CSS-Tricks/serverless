f9aaa5632d92f9b280368ced052dadeccf8ffafd
Chris Coyier <chriscoyier@gmail.com>
2019-10-18 17:18:58 -0700

    matching ports

--- snapshots.js
+++ snapshots.js
@@ -1,7 +1,7 @@
 const PercyScript = require("@percy/script");
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto("http://localhost:8080/");
+  await page.goto("http://localhost:9000/");
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor("body.homepage");
   await percySnapshot("homepage");
