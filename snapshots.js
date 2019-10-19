dc0c9a482a2b226af41486d77a24c458bea2f386
Chris Coyier <chriscoyier@gmail.com>
2019-10-18 17:25:23 -0700

    will percyscript find this?

--- snapshots.js
+++ snapshots.js
@@ -3,6 +3,6 @@ const PercyScript = require("@percy/script");
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto("http://localhost:9000/");
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor("body.homepage");
+  await page.waitFor("#gatsby-focus-wrapper");
   await percySnapshot("homepage");
 });
