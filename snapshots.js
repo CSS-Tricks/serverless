e158050aab4d3abd4d6171f7c476ca50619f1c37
Chris Coyier <chriscoyier@gmail.com>
2019-10-29 13:15:56 -0700

    waiting for idle network before screenshot

--- snapshots.js
+++ snapshots.js
@@ -1,13 +1,19 @@
 const PercyScript = require("@percy/script");
 
 PercyScript.run(async (page, percySnapshot) => {
+  // the `page` object here is Puppeteer!
+  // https://github.com/GoogleChrome/puppeteer
   await page.goto("http://localhost:9000/");
+
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor("#gatsby-focus-wrapper");
+
   await percySnapshot("homepage");
 
-  await page.goto("http://localhost:9000/services/major");
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor("#gatsby-focus-wrapper");
+  await page.goto("http://localhost:9000/services/major", {
+    waitUntil: "networkidle0"
+  });
+
   await percySnapshot("Services/Major");
 });
