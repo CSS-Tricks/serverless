ae932ff742ad04dd772e4a058de118e4431609f4
Chris Coyier <chriscoyier@gmail.com>
2021-11-28 06:51:50 -0800

    remove public

--- public/component---src-pages-404-js-a89e334cdecafc98b57c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("vrFN");t.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a89e334cdecafc98b57c.js.map
